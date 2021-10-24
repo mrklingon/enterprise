ea910bb1f444aaf24ff1f90217d994d1aa3bf75f
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-10-23 20:17:25 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,53 @@
+function Splash () {
+    Ent = images.createBigImage(`
+        . . . . . . . . . .
+        # # # # . # # # # #
+        . . # . . . # . . .
+        . . . # # # # # . .
+        . . . . . . . . . .
+        `)
+    Ent2 = images.createBigImage(`
+        # . . . . . . # . .
+        . . . . . . . . . .
+        . . # # # . . . # .
+        . # # # # # . . . .
+        # # # # # # # . . .
+        `)
+    Ent.scrollImage(1, 200)
+    Ent2.scrollImage(1, 200)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+}
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index <= 4; index++) {
+        Ship.move(1)
+        basic.pause(100)
+        Ship.ifOnEdgeBounce()
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    Splash()
+    Ship.set(LedSpriteProperty.X, randint(0, 4))
+    Ship.set(LedSpriteProperty.Y, randint(0, 4))
+})
+input.onButtonPressed(Button.B, function () {
+    Dir += 1
+    if (8 == Dir) {
+        Dir = 0
+    }
+    basic.showNumber(Dir)
+    Ship.set(LedSpriteProperty.Direction, 45 * Dir)
+})
+let Ent2: Image = null
+let Ent: Image = null
+let Dir = 0
+let Ship: game.LedSprite = null
+Splash()
+Ship = game.createSprite(2, 2)
+Dir = 0
+Ship.set(LedSpriteProperty.Direction, 45 * Dir)
