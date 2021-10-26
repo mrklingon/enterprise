eb4200a1b318295a593f39ac649f349d968e7f52
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-10-26 15:12:04 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -32,7 +32,20 @@ input.onButtonPressed(Button.A, function () {
         Ship.move(1)
         basic.pause(100)
         Ship.ifOnEdgeBounce()
+        if (9 < Planets) {
+            if (Ship.isTouching(Asteroid)) {
+                Moves += -2
+                basic.showIcon(IconNames.No)
+                basic.pause(100)
+            }
+        }
         if (Ship.isTouching(Planet)) {
+            Moves += 2
+            Planets += 1
+            if (10 == Planets) {
+                Asteroid = game.createSprite(randint(0, 4), randint(0, 4))
+                Asteroid.set(LedSpriteProperty.Direction, randint(0, 360))
+            }
             for (let index2 = 0; index2 < 2; index2++) {
                 game.addScore(5)
                 basic.showIcon(IconNames.SmallDiamond)
@@ -67,8 +80,10 @@ input.onButtonPressed(Button.B, function () {
     Ship.set(LedSpriteProperty.Direction, 45 * Dir)
 })
 let Planet: game.LedSprite = null
+let Asteroid: game.LedSprite = null
 let Ent2: Image = null
 let Ent: Image = null
+let Planets = 0
 let Moves = 0
 let Dir = 0
 let Ship: game.LedSprite = null
@@ -77,4 +92,14 @@ Ship = game.createSprite(2, 2)
 Dir = 0
 Ship.set(LedSpriteProperty.Direction, 45 * Dir)
 mkPlanet()
-Moves = 100
+Moves = 30
+Planets = 0
+basic.forever(function () {
+    basic.pause(250)
+    if (Planets > 9) {
+        for (let index = 0; index < 4; index++) {
+            Asteroid.move(1)
+            Asteroid.ifOnEdgeBounce()
+        }
+    }
+})
